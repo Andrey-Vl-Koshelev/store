1e78b5ae6a1f4d601a8ace7142c4e2fca704441a
Andrey <111304647+Andrey-Vl-Koshelev@users.noreply.github.com>
2023-06-11 13:43:32 +0400

    учебный проект четвертое добавление

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
 from django.urls import reverse
+from products.models import Basket
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
@@ -34,6 +36,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -44,7 +47,23 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    baskets = Basket.objects.filter(user=request.user)
+    # total_sum = sum(basket.sum() for basket in baskets)
+    # total_quantity = sum(basket.quantity for basket in baskets)
+
+    # total_sum = 0
+    # total_quantity = 0
+    # for basket in baskets:
+    #     total_sum += basket.sum()
+    #     total_quantity += basket.quantity
+
+    context = {'title': 'Store - Профиль',
+               'form': form,
+               'baskets': baskets,
+               # 'total_sum': sum(basket.sum() for basket in baskets),
+               # 'total_quantity': sum(basket.quantity for basket in baskets),
+               }
     return render(request, 'users/profile.html', context)
 
 
